6fd925b4db76f61bee764118229e7e979c321e41
Seongjoo-Shin <ssj3357@naver.com>
2022-04-26 11:08:48 +0900

    0426 신성주 - mybatis 재설정 .mybatis 삭제, NoticeBoards -> NoticeBoardDto

--- src/main/java/com/mycompany/webapp/dto/NoticeBoardDto.java
+++ src/main/java/com/mycompany/webapp/dto/NoticeBoardDto.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class NoticeBoards {
+public class NoticeBoardDto {
 	private int noticeNo;
 	private String noticeWriter;
 	private String noticeTitile;
